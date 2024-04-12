f41a43b5af0905114c905ee1dbdaa56a43272174
EgdarS <egi.shaqiri99@gmail.com>
2024-04-12 23:10:56 +0200

    adding homework

--- python/fundementals/fundementals/nested_dictionaries_lists.py
+++ python/fundementals/fundementals/nested_dictionaries_lists.py
@@ -29,5 +29,35 @@ students = [
 
 def iterateDictionary(some_list):
     for student in some_list:
-        for key in student:
-            print(f"{key}: {student[key]}")
+        output = ""
+        for key, value in student.items():
+            output += f"{key} - {value}, "
+        print(output[:-2])  # to remove the trailing comma and space
+# iterateDictionary(students)        
+
+#3. Get Values From a List of Dictionaries
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def iterateDictionary2(key_name, some_list):
+    for dictionary in some_list:
+        print(dictionary.get(key_name))
+
+# iterateDictionary2('first_name', students)
+# iterateDictionary2('last_name', students)
+
+#4. Iterate Through a Dictionary with List Values
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print(f"{len(value)} {key.upper()}")
+        for item in value:
+            print(item)
+printInfo(dojo)
