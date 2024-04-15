8cf21e97bad34308e791016dfabac59a5f3f3342
EgdarS <egi.shaqiri99@gmail.com>
2024-04-15 16:55:19 +0200

    oop homework

---
+++ python/fundementals/oop/bankAccount.py
@@ -0,0 +1,32 @@
+class bankAccount:
+    def __init__(self, amount=0, accountHolder='Default', pin='1234'):
+        self.amount = amount
+        self.pin= pin
+        self.accountHolder=accountHolder
+
+    def showAmount(self,pin):
+        if self.pin !=pin:
+            print('Incorrect pin! Try again.')
+            return False
+        print(f'You have ${self.amount} in your bank account, dear {self.accountHolder}')
+        return self
+
+    def deposit(self, pin, vleraRe):
+        if self.pin != pin:
+            print('Incorrect pin! Try again.')
+            return False
+        self.amount=self.amount+int(vleraRe)
+        print(f'You have succesfully deposited ${self.amount} in your bank account.')
+
+    def withdraw(self, pin, vleraTerheqjes):
+        if self.pin !=pin:
+            print('Incorrect pin! Try again.')
+            return False
+        if self.amount > self.amount - int(vleraTerheqjes):
+            self.amount=self.amount-int(vleraTerheqjes)
+            print(f'You have succesfully withdrawed ${vleraTerheqjes} in your bank account.')
+            print(f'Current balance: ${self.amount}')
+            return self
+        else:
+            print(f'Insufficient funds. Current balance: ${self.amount}')
+            return self  
---
+++ python/fundementals/oop/user.py
@@ -0,0 +1,23 @@
+from bankAccount import bankAccount
+class User:
+    type = 'human'
+    def __init__(self, firstName='Default', lastName='User', email='test@test.com', amount=0, pin='1234'):
+        self.email=email
+        self.bankAccount=bankAccount(accountHolder=email, pin=pin)
+
+    def getFirstName(self):
+        print(f'Your name is {self.firstName}')
+        return self
+    def getLastName(self):
+        print(f'Your last name is {self.lastName}')
+        return self
+    def getEmail(self):
+        print(f'Your email is{self.email}')
+        return self
+    
+
+egi=User('Egdar', 'Shaqiri', 'egi.shaqiri99@gmail.com', pin='1234')
+
+#egi.bankAccount.deposit(input('Enter pin: '), input('How much do you want to deposit? '))
+
+egi.bankAccount.withdraw(input('Enter pin: '), input('How much do you want to withdraw? '))
